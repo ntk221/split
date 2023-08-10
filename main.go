2d26ec33a647f41042fe8dee21cb5b0e3a23dc91
ntk221 <nittakdub@gmail.com>
2023-08-10 15:27:42 +0900

    line_countの実装途中まで完了

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"flag"
 	"log"
+	"os"
+	"bufio"
+	"flag"
+	"io"
 )
 
 var (
-	line_count = flag.Int("l", 1000, "行数を指定してください")
+	lineCount = flag.Int("l", 1000, "行数を指定してください")
 )
 
 func main() {
 	flag.Parse()
 
-	fmt.Printf("line_count is %d\n",*line_count)
+	fmt.Printf("line_count is %d\n", *lineCount)
 	args := flag.Args()
 	fmt.Println(args)
 
@@ -28,5 +30,36 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	fmt.Println(file)
+
+	partNum := 0
+	outputPrefix := "x"
+
+	reader := bufio.NewReader(file)
+	for {
+		partName := fmt.Sprintf("%s%03d", outputPrefix, partNum)
+		partFileName := fmt.Sprintf("%s.txt", partName)
+		partFile, err := os.Create(partFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i := 0; i < *lineCount; i++ {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					os.Remove(partFileName)
+					fmt.Println("ファイル分割が終了しました")
+					return
+				}
+				fmt.Println("行を読み込めませんでした:", err)
+				return
+			}
+			
+			partFile.WriteString(line)
+		}
+		defer partFile.Close()
+
+		partNum++
+	}
 }
+
