aa10850c23a8a1b5c05f25697400567c0386a644
ntk221 <nittakdub@gmail.com>
2023-08-10 17:30:14 +0900

    [#1]splitUsingLineCountを実装, split関数から呼び出すようにした

--- main.go
+++ main.go
@@ -9,14 +9,75 @@ import (
 	"io"
 )
 
-var (
-	lineCount = flag.Int("l", 1000, "行数を指定してください")
+const (
+	FILE_LIMIT = 677 // 27^2
+	DEFAULT_PREFIX = "x"
 )
+
+var (
+	lineCountOption = flag.Int("l", 1000, "行数を指定してください")
+)
+
+func splitUsingLineCount(lineCount int, outputPrefix string, partNum int, file *os.File) {
+
+	reader := bufio.NewReader(file)
+	for {
+		if partNum >= FILE_LIMIT {
+			log.Fatal("too many files")
+		}
+		partName := fmt.Sprintf("%s%03d", outputPrefix, partNum)
+		partFileName := fmt.Sprintf("%s.txt", partName)
+		partFile, err := os.Create(partFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i := 0; i < lineCount; i++ {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					// 一度も読み込めない時はCreateしたファイルを消す
+					if i == 0 {
+						os.Remove(partFileName)
+					}
+					fmt.Println("ファイル分割が終了しました")
+					return 
+				} else {
+					fmt.Println("行を読み込めませんでした:", err)
+					log.Fatal(err)
+				}
+			}
+
+			partFile.WriteString(line)
+		}
+		partFile.Close()
+
+		partNum++
+	}
+}
+
+func split(args []string, file *os.File) {
+	// optionによって処理を分岐する
+	flag.Parse()
+	lineCount := *lineCountOption
+	// chunkCount := *chunkCountOption
+	// byteCount := *byteCountOption
+
+	partNum := 0
+	outputPrefix := DEFAULT_PREFIX
+	// 引数でprefixが指定されている場合はそれを使う
+	if len(args) > 1 {
+		outputPrefix = args[1]
+	}
+	
+	// chunkCountが設定されてたら -> splitUsingChunkCount
+	// byteCountが設定されてたら -> splitUsingByteCount
+
+	splitUsingLineCount(lineCount, outputPrefix, partNum, file)
+}
 
 func main() {
 	flag.Parse()
-
-	fmt.Printf("line_count is %d\n", *lineCount)
 	args := flag.Args()
 	fmt.Println(args)
 
@@ -31,35 +92,6 @@ func main() {
 	}
 	defer file.Close()
 
-	partNum := 0
-	outputPrefix := "x"
-
-	reader := bufio.NewReader(file)
-	for {
-		partName := fmt.Sprintf("%s%03d", outputPrefix, partNum)
-		partFileName := fmt.Sprintf("%s.txt", partName)
-		partFile, err := os.Create(partFileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for i := 0; i < *lineCount; i++ {
-			line, err := reader.ReadString('\n')
-			if err != nil {
-				if err == io.EOF {
-					os.Remove(partFileName)
-					fmt.Println("ファイル分割が終了しました")
-					return
-				}
-				fmt.Println("行を読み込めませんでした:", err)
-				return
-			}
-			
-			partFile.WriteString(line)
-		}
-		defer partFile.Close()
-
-		partNum++
-	}
+	split(args, file)
 }
 
