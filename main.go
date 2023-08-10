fd1dbacb188fecae7ae9defdc0484291049dca38
ntk221 <nittakdub@gmail.com>
2023-08-10 13:49:51 +0900

    flag packageを使ってoptionの解析機能を追加した

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"os"
 	"fmt"
+	"flag"
+)
+
+var (
+	line_count = flag.Int("l", 1000, "行数を指定してください")
 )
 
 func main() {
+	flag.Parse()
+
+	fmt.Printf("line_count is %d\n",*line_count)
 	args := os.Args
 	fmt.Println(args)
 }
