003fba8813414f99159aeb0c29e69ef2e6968ca6
ntk221 <nittakdub@gmail.com>
2023-09-05 18:18:29 +0900

    tdtの形式にした

--- split_test.go
+++ split_test.go
@@ -17,23 +17,84 @@ func init() {
 	flag.BoolVar(&flagUpdate, "update", false, "update golden files")
 }
 
-func Test(t *testing.T) {
-	dir := t.TempDir()
-	option := option.LineCount(1)
-	input := strings.NewReader("Line1\nLine2\n")
-	s := splitter.New(option, "x")
-
-	cli := &splitter.CLI{
-		Input:     input,
-		OutputDir: dir,
-		Splitter:  s,
+func TestSplitterCLI(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		lineCount int
+		expected  string
+	}{
+		{
+			name:      "Test Case 1",
+			input:     "Line1\nLine2\n",
+			lineCount: 1,
+			expected:  "expected_output_1.txtar",
+		},
+		// Add more test cases here as needed
 	}
 
-	cli.Run()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir := t.TempDir()
+			option := option.LineCount(tt.lineCount)
+			input := strings.NewReader(tt.input)
+			s := splitter.New(option, "x")
 
-	got := golden.Txtar(t, dir) // txtar形式に固める
+			cli := &splitter.CLI{
+				Input:     input,
+				OutputDir: dir,
+				Splitter:  s,
+			}
 
-	if diff := golden.Check(t, flagUpdate, "testdata", "mytest", got); diff != "" { // testdata 以下のmytestとdiffをとる
-		t.Error(diff)
+			cli.Run()
+
+			got := golden.Txtar(t, dir)
+
+			if diff := golden.Check(t, flagUpdate, "testdata", tt.expected, got); diff != "" {
+				t.Errorf("Test case %s failed:\n%s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func Test(t *testing.T) {
+	tests := []struct {
+		name         string
+		input        string
+		option       option.Command
+		outputPrefix string
+		wantData     string
+	}{
+		{
+			name:         "Test Case 1",
+			input:        "Line1\nLine2\n",
+			option:       option.NewLineCount(1),
+			outputPrefix: "x",
+			wantData:     "mytest",
+		},
+		// Add more test cases here as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir := t.TempDir()
+			option := tt.option
+			input := strings.NewReader(tt.input)
+			s := splitter.New(option, tt.outputPrefix)
+
+			cli := &splitter.CLI{
+				Input:     input,
+				OutputDir: dir,
+				Splitter:  s,
+			}
+
+			cli.Run()
+
+			got := golden.Txtar(t, dir)
+
+			if diff := golden.Check(t, flagUpdate, "testdata", tt.wantData, got); diff != "" {
+				t.Errorf("Test case %s failed:\n%s", tt.name, diff)
+			}
+		})
 	}
 }
